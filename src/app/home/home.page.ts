47d7bc09005fc3a438840fa9b6a3c22258b0e067
Egor Santamaria <esantamaria@vunkers.com>
2024-04-22 09:19:02 +0200

    Retrieve cryptos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CryptoService } from '../services/Crypto/crypto.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private _cryptoService : CryptoService) {}
+
+  getCryptos () {
+    return this._cryptoService.getCryptos();
+  }
 
 }
--- src/app/services/Crypto/crypto.service.ts
+++ src/app/services/Crypto/crypto.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Crypto } from 'src/app/models/crypto';
 
@@ -6,13 +7,25 @@ import { Crypto } from 'src/app/models/crypto';
 })
 export class CryptoService {
 
-  private _api_url : string = 'api.coincap.io/v2/assets';
+  private _api_url : string = 'https://api.coincap.io/v2/assets';
   private _cryptos : Crypto [] = [];
 
-  constructor() { }
+  constructor(private _httpClient : HttpClient) { 
+
+    this.retrieveCryptos();
+  }
 
   retrieveCryptos () {
+    this._httpClient.get(this._api_url).subscribe({
+      next: (data : any) => {
+        const getData = data.data;
+        this._cryptos = [...getData];
+      }
+    })
+  }
 
+  getCryptos () {
+    return this._cryptos;
   }
 
 }
