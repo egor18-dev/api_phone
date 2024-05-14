930c460bd3354a6e81ae9f1e4141f61891b230f5
egorsr <=>
2024-05-14 18:13:27 +0200

    Filters

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { Crypto } from '../models/crypto';
 import jsQR from 'jsqr';
 import { Router } from '@angular/router';
 import { DataService } from '../services/Data/data.service';
+import { CameraService } from '../services/Camera/camera.service';
 
 @Component({
   selector: 'app-home',
@@ -16,15 +17,25 @@ export class HomePage {
   isOpened: boolean = false;
   cryptos: Crypto[] = [];
   cryptosCopy: Crypto[] = [];
+  private _selectedValue !: string;
 
   @ViewChild('video', { static: false }) videoElement!: ElementRef;
   video: any;
 
   public crypto: string = '';
 
+  /*
+    - COMENTARI EXAMEN-
+    
+    Al meu dispositiu mobil no es valid el lector de BARRES QR,
+    per això o faig fer d'aquella manera amb el canvas, i ara no puc
+    provar si funciona del tot. Pero hauria d'anar aixi.
+  */
+
   constructor(private _cryptoService: CryptoService,
     private _router : Router,
-    private _dataService : DataService
+    private _dataService : DataService,
+    private _cameraService : CameraService
   ) { }
 
   ngOnInit() {
@@ -49,7 +60,19 @@ export class HomePage {
   }
 
   getCryptos() {
-    return this._cryptoService.getCryptos();
+    if(this._selectedValue){
+
+      if(this._selectedValue === 'major'){
+        return this._cryptoService.getCryptos().filter((crypto: Crypto) => Number(crypto.changePercent24Hr) > 0);
+      }else if(this._selectedValue === 'menor'){
+        return this._cryptoService.getCryptos().filter((crypto: Crypto) => Number(crypto.changePercent24Hr) < 0);
+      }else{
+        return this._cryptoService.getCryptos();
+      }
+
+    }else{
+      return this._cryptoService.getCryptos();
+    }
   }
 
   getColor(price: string) {
@@ -61,76 +84,20 @@ export class HomePage {
       this.cryptos = this.cryptosCopy;
     }
 
-    console.log(this.crypto);
-    console.log(this.cryptos);
-
     this.cryptos = this.cryptos.filter((crypto: Crypto) => crypto.name.toLowerCase().startsWith(this.crypto.toLowerCase()));
   }
 
   scan() {
-    this.isOpened = true;
-    this.video = this.videoElement.nativeElement;
-
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-      .then(stream => {
-        this.video.srcObject = stream;
-        this.video.play();
-        this.scanForQrCode();
-      })
-      .catch(error => {
-        console.error('Error accessing camera:', error);
-      });
+    this._cameraService.scan().then((crypto : any) => {
+        /* 
+          -- No se si crypto ja es directament el nom de la criptomoneda perque no tinc com 
+          provar-ho
+        */
+        this._dataService.writeSecretFile(crypto);
+    });   
   }
-
-  scanForQrCode() {
-    const canvasElement = document.createElement('canvas');
-    const canvas = canvasElement.getContext('2d')!;
-    const video = this.video;
-  
-    const self = this;
-  
-    function captureFrame() {
-      canvasElement.width = video.videoWidth;
-      canvasElement.height = video.videoHeight;
-  
-      if (video.videoWidth && video.videoHeight) {
-        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-  
-        const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-  
-        const code = jsQR(imageData.data, imageData.width, imageData.height);
-  
-        if (code) {
-          self._dataService.writeSecretFile(code.data);
-          self._router.navigate([`/view/${code.data}`]);
-          self.isOpened = false;
-
-          const stream = video.srcObject as MediaStream;
-          const tracks = stream.getTracks();
-          tracks.forEach(track => {
-            track.stop();
-          });
-        } else {
-          requestAnimationFrame(captureFrame);
-        }
-      } else {
-        requestAnimationFrame(captureFrame);
-      }
-    }
-    captureFrame();
-  }
-  
-
-  
 
   read(event: any) {
-    const selectedValue = event.detail.value;
-    this.cryptos = this.cryptosCopy;
-
-    if (selectedValue === 'major') {
-      this.cryptos = this.cryptos.filter((crypto: Crypto) => Number(crypto.changePercent24Hr) > 0);
-    } else if (selectedValue === 'menor') {
-      this.cryptos = this.cryptos.filter((crypto: Crypto) => Number(crypto.changePercent24Hr) < 0);
-    }
+    this._selectedValue = event.detail.value;
   }
 }
--- src/app/services/Camera/camera.service.ts
+++ src/app/services/Camera/camera.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Barcode, BarcodeFormat, BarcodeScanner, IsSupportedResult, ScanOptions, ScanResult } from '@capacitor-mlkit/barcode-scanning';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CameraService {
 
-  constructor() { }
+  private _barcodes !: Barcode[];
+  private _supported: boolean;
+
+  constructor() {
+    this._supported = false;
+    this.isSupported();
+  }
+
+  isSupported(): void {
+    BarcodeScanner.isSupported().then(
+      (result: IsSupportedResult) => {
+        this._supported = result.supported;
+      }
+    ).catch(
+      (error: any) => {
+        this._supported = false;
+      }
+    ).finally(() => {});
+  }
+
+  get supported(): boolean {
+    return this._supported
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const permissions: any = await BarcodeScanner.requestPermissions();
+    return permissions.camera === 'granted' || permissions.camera === 'limited';
+  }
+
+  async scan(): Promise<boolean> {
+    const granted = await this.requestPermissions();
+    if(granted) { 
+      const options: ScanOptions = {
+        formats: [BarcodeFormat.Ean13, BarcodeFormat.QrCode]
+      }
+
+      const result: ScanResult = await BarcodeScanner.scan(options);
+      this._barcodes = result.barcodes;
+
+      return true;
+    }
+    this._barcodes = [];
+    return false;
+  }
+
+  get barcodes(): Barcode[] {
+    return this._barcodes;
+  }
+
 }
